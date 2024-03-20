7f6a80412b2141b20cf0a4338a7af1dbb6a7da08
dafafikrie <151342882+dafafikrie@users.noreply.github.com>
2024-03-20 10:51:11 +0700

    Add files via upload

---
+++ Class3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa{
+	private:
+		string nama, npm;
+
+}
+int main(){
+	Mahasiswa Mhs;
+
+	cin >> Mhs.nama >> Mhs.npm;
+
+	Mhs.Perkenalan();
+}
